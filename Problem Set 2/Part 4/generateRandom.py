f09f5e6ffa17f72e317c0b52d040d148ffb01990
marmik pandya <marmikpandya@gmail.com>
2016-02-21 16:06:51 -0500

    training set generation

--- Problem Set 2/Part 4/generateRandom.py
+++ Problem Set 2/Part 4/generateRandom.py
@@ -1,9 +1,27 @@
 from random import randint
-n, N = 2000, 2000
-points = {(randint(0, n), randint(0, n)) for i in range(N)}
-while len(points) < N:
-    points |= {(randint(0, n), randint(0, n))}
-     
-points = list(list(x) for x in points)
 
-print(points)
+#Starts with 0,0
+#Pics up a random point from outside the coverage range from access point at that point
+#and adds it to the list
+
+xCoverage, yCoverage = 0, 0
+finalPoints = list()
+for i in range(10):
+	point = {(randint(xCoverage,xCoverage+2),randint(yCoverage,yCoverage+2))}
+	finalPoints.append(point)
+	for j in range(10):
+		point = {(randint(xCoverage+1,xCoverage+2),randint(yCoverage+1,yCoverage+2))}
+		finalPoints.append(point)
+		yCoverage= yCoverage+2
+	xCoverage, yCoverage=xCoverage+2, 0
+
+print (finalPoints)
+
+print("#################################################")
+
+for i in range(len(finalPoints)):
+	point = {(randint(0,20),randint(0,20))}
+	finalPoints.append(point)
+
+print (len(finalPoints))
+
