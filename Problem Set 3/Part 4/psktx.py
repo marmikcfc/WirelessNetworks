aae222a191609a0b87bd26e5acfbd51d038b5a38
marmik <marmikpandya@gmail.com>
2016-03-05 08:28:21 +0000

    Transmit using hackrf

---
+++ Problem Set 3/Part 4/psktx.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Psktx
+# Generated: Sat Mar  5 08:25:51 2016
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt4 import Qt
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import qtgui
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from gnuradio.filter import pfb
+from grc_gnuradio import blks2 as grc_blks2
+from optparse import OptionParser
+import numpy
+import osmosdr
+import sip
+import sys
+import time
+
+
+class psktx(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Psktx")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Psktx")
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "psktx")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.sps = sps = 2
+        self.samp_rate = samp_rate = 1e6
+        self.my_constellation = my_constellation = digital.constellation_calcdist(([-1,1]), ([0, 1]), 2, 1).base()
+        self.freq = freq = 2.4e9
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+        	1024, #fftsize
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	freq, #fc
+        	samp_rate, #bw
+        	"", #name
+        	True, #plotfreq
+        	True, #plotwaterfall
+        	True, #plottime
+        	True, #plotconst
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_sink_x_0_win)
+        
+        self.qtgui_sink_x_0.enable_rf_freq(False)
+        
+        
+          
+        self.pfb_arb_resampler_xxx_0 = pfb.arb_resampler_ccf(
+        	  2,
+                  taps=(filter.firdes.root_raised_cosine(32,32,1.0,0.35,11*32*sps)),
+        	  flt_size=32)
+        self.pfb_arb_resampler_xxx_0.declare_sample_delay(0)
+        	
+        self.osmosdr_sink_0 = osmosdr.sink( args="numchan=" + str(1) + " " + "hackrf=0x000000000x000000000x457863c80x320c831f" )
+        self.osmosdr_sink_0.set_sample_rate(samp_rate)
+        self.osmosdr_sink_0.set_center_freq(freq, 0)
+        self.osmosdr_sink_0.set_freq_corr(0, 0)
+        self.osmosdr_sink_0.set_gain(10, 0)
+        self.osmosdr_sink_0.set_if_gain(20, 0)
+        self.osmosdr_sink_0.set_bb_gain(20, 0)
+        self.osmosdr_sink_0.set_antenna("", 0)
+        self.osmosdr_sink_0.set_bandwidth(0, 0)
+          
+        self.digital_diff_encoder_bb_0 = digital.diff_encoder_bb(my_constellation.arity())
+        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc((my_constellation.points()), my_constellation.dimensionality())
+        self.blocks_packed_to_unpacked_xx_0 = blocks.packed_to_unpacked_bb(my_constellation.bits_per_symbol(), gr.GR_MSB_FIRST)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((0.05, ))
+        self.blks2_packet_encoder_0 = grc_blks2.packet_mod_b(grc_blks2.packet_encoder(
+        		samples_per_symbol=2,
+        		bits_per_symbol=my_constellation.bits_per_symbol(),
+        		preamble="",
+        		access_code="",
+        		pad_for_usrp=False,
+        	),
+        	payload_length=0,
+        )
+        self.analog_random_source_x_0 = blocks.vector_source_b(map(int, numpy.random.randint(0, 256, 100000)), True)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_random_source_x_0, 0), (self.blks2_packet_encoder_0, 0))    
+        self.connect((self.blks2_packet_encoder_0, 0), (self.blocks_packed_to_unpacked_xx_0, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.osmosdr_sink_0, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.qtgui_sink_x_0, 0))    
+        self.connect((self.blocks_packed_to_unpacked_xx_0, 0), (self.digital_diff_encoder_bb_0, 0))    
+        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.pfb_arb_resampler_xxx_0, 0))    
+        self.connect((self.digital_diff_encoder_bb_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))    
+        self.connect((self.pfb_arb_resampler_xxx_0, 0), (self.blocks_multiply_const_vxx_0, 0))    
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "psktx")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+
+    def get_sps(self):
+        return self.sps
+
+    def set_sps(self, sps):
+        self.sps = sps
+        self.pfb_arb_resampler_xxx_0.set_taps((filter.firdes.root_raised_cosine(32,32,1.0,0.35,11*32*self.sps)))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.qtgui_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
+        self.osmosdr_sink_0.set_sample_rate(self.samp_rate)
+
+    def get_my_constellation(self):
+        return self.my_constellation
+
+    def set_my_constellation(self, my_constellation):
+        self.my_constellation = my_constellation
+        self.digital_chunks_to_symbols_xx_0.set_symbol_table((self.my_constellation.points()))
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.qtgui_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
+        self.osmosdr_sink_0.set_center_freq(self.freq, 0)
+
+
+def main(top_block_cls=psktx, options=None):
+
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
